0db80e056c4ba6b7ed204b42ee5774b5c36e24a5
granpepito <alexandre.goncalvesm@gmail.com>
2023-05-25 01:12:36 +0200

    :sparkles: New side bar.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,49 @@
+import { useState } from 'react';
 import { Bars3Icon } from '@heroicons/react/24/outline';
-import styles from '/src/assets/styles/header.module.css';
+import SideBar from './SideBar';
 
-function openNavBar() {
-	console.log('NavBar open');
-}
+import styles from '/src/assets/styles/header.module.css';
+import inputStyles from '/src/assets/styles/input.module.css';
+import iconStyles from '/src/assets/styles/icon.module.css';
 
 export default function Header() {
+	const [isSideBarOpen, setSideBarState] = useState(false);
+
+	function openNavBar() {
+		if (isSideBarOpen) {
+			const openSideBarBtn = document.getElementById('openSideBarBtn');
+			openSideBarBtn.focus();
+			openSideBarBtn.blur();
+		} else {
+			const closeSideBarBtn = document.getElementById('closeSideBarBtn');
+			closeSideBarBtn.focus();
+			closeSideBarBtn.blur();
+		}
+		setSideBarState(!isSideBarOpen);
+	}
+
 	return (
 		<>
 			<header className={styles.header}>
-				<button onClick={openNavBar}>
-					<Bars3Icon className={styles.hamburgerIcon} />
+				<button
+					id='openSideBarBtn'
+					className={inputStyles.buttonIcon}
+					onClick={openNavBar}
+				>
+					<Bars3Icon className={iconStyles.icon} />
 				</button>
 
 				<h1 className={styles.websiteName}>
 					<a href='#'>Fontry</a>
 				</h1>
+				<div
+					style={{
+						height: '40px',
+						width: '36px',
+					}}
+				></div>
 			</header>
+			<SideBar handleClick={openNavBar} isOpen={isSideBarOpen} />
 		</>
 	);
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,88 @@
+import { XMarkIcon, SunIcon, MoonIcon } from '@heroicons/react/24/outline';
+import {
+	SunIcon as SunIconSolid,
+	MoonIcon as MoonIconSolid,
+} from '@heroicons/react/24/solid';
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid';
+
+// import Pair from '/src/utils/Pair';
+// import Font from '/src/utils/Font';
+
+import styles from '/src/assets/styles/sidebar.module.css';
+import inputStyles from '/src/assets/styles/input.module.css';
+import iconStyles from '/src/assets/styles/icon.module.css';
+
+/**
+ *
+ * @param {{ handleClick: Function, isOpen: boolean}} param0
+ * @
+ * @returns
+ */
+export default function SideBar({ handleClick, isOpen }) {
+	const sideBarContainerOpen = isOpen ? styles.open : '';
+
+	return (
+		<div
+			className={[styles.sideBarContainer, sideBarContainerOpen].join(' ')}
+			{...(!isOpen ? 'disabled' : '')}
+		>
+			<div className={styles.sideBarTopContainer}>
+				<button
+					id='closeSideBarBtn'
+					className={inputStyles.buttonIcon}
+					onClick={handleClick}
+				>
+					<XMarkIcon className={iconStyles.icon} />
+				</button>
+				<div className={styles.themeSelectorContainer}></div>
+			</div>
+			<nav className={styles.sideBarMidContainer}>
+				<h2>Combinaisons Enregistrées</h2>
+				<ul className={styles.savedPairsList}>
+					<SavedPair />
+					<SavedPair />
+					<SavedPair />
+					<SavedPair />
+				</ul>
+			</nav>
+			<footer className={styles.sideBarBottomContainer}></footer>
+		</div>
+	);
+}
+
+/**
+ *
+ * @returns
+ */
+function ThemeSelector() {
+	return (
+		<>
+			<SunIcon className={[iconStyles.icon, iconStyles.sunIcon].join(' ')} />
+			<input className={styles.themeSelector} type='checkbox' />
+			<MoonIcon className={[iconStyles.icon, iconStyles.moonIcon].join(' ')} />
+		</>
+	);
+}
+
+/**
+ *
+ * @param {} param0
+ * @returns
+ */
+function SavedPair({}) {
+	return (
+		<li className={styles.savedPair}>
+			<p>
+				1. <span>Police 1</span>
+			</p>
+			<p>
+				2. <span>Police 2</span>
+			</p>
+			<button className={inputStyles.buttonIcon}>
+				<EllipsisVerticalIcon
+					className={[iconStyles.icon, styles.ellipsisVertIcon].join(' ')}
+				/>
+			</button>
+		</li>
+	);
+}
