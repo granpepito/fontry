3bc4316ad7d713f950f7e32bc0cc936a5ed28ab1
granpepito <alexandre.goncalvesm@gmail.com>
2023-06-03 13:39:55 +0200

    Revert ":truck: Renamed AlphaComparison component to AlphaNumComparison."
    This reverts commit 4af2425d1b18908fb52f63d54ac58b122108efcb.

--- src/components/ComparisonPanel.jsx
+++ src/components/ComparisonPanel.jsx
@@ -1,9 +1,10 @@
-import { AlphaNumComparison } from './AlphaNumComparison';
+import { AlphaComparison } from './AlphaComparison';
 import { TextualExampleComparison } from './TextualExampleComparison';
 
 export function ComparisonPanel({ currentSection }) {
 	switch (currentSection) {
 		case 'alphanum': {
+			return <AlphaComparison />;
 		}
 		default:
 		case 'textual': {
