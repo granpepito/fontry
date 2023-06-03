61f9eacaf8bfa609ad770e21023cac91aa13951b
granpepito <alexandre.goncalvesm@gmail.com>
2023-06-03 13:58:17 +0200

    :construction: Added TextualElement component.

--- src/components/TextualExampleComparison.jsx
+++ src/components/TextualExampleComparison.jsx
@@ -12,3 +12,66 @@ export function TextualExampleComparison() {
 		</section>
 	);
 }
+
+function TextualElement({ font, type }) {
+	const typesettingFontNumber = (fontNumber) => (
+		<p className={styles.typesettingFontNumber}>{fontNumber}</p>
+	);
+	switch (type) {
+		case 'title': {
+			return (
+				<div className={[styles.textualElement, styles.titleElement].join(' ')}>
+					{typesettingFontNumber('Police 1')}
+					<h1 contentEditable='true'>
+						<span>TITRE</span>&nbsp; Article
+					</h1>
+				</div>
+			);
+		}
+		case 'subtitle': {
+			return (
+				<div
+					className={[styles.textualElement, styles.subTitleElement].join(' ')}
+				>
+					{typesettingFontNumber('Police 1')}
+					<h3 contentEditable='true'>
+						<span contentEditable='false'>SOUS-TITRE</span>&nbsp; Article
+					</h3>
+				</div>
+			);
+		}
+		case 'paragraph': {
+			return (
+				<div
+					className={[styles.textualElement, styles.paragraphElement].join(' ')}
+				>
+					{typesettingFontNumber('Police 2')}
+					<p>PARAGRAPHE</p>
+					<p contentEditable='true'></p>
+				</div>
+			);
+		}
+		case 'actions': {
+			return (
+				<div
+					className={[styles.textualElement, styles.actionsElement].join(' ')}
+				>
+					{typesettingFontNumber('Police 2')}
+
+					<p>
+						<button></button>
+						<span>
+							<a></a>
+						</span>
+						<span>
+							<a></a>
+						</span>
+					</p>
+				</div>
+			);
+		}
+		default: {
+			return null;
+		}
+	}
+}
