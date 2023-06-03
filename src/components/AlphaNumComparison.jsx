bafc2f85b28aac1d9bce9e1cfc7da2e1c0a96b12
granpepito <alexandre.goncalvesm@gmail.com>
2023-06-03 14:01:26 +0200

    :construction: Added AlphaNumElement component and structure to the section.

--- src/components/AlphaNumComparison.jsx
+++ src/components/AlphaNumComparison.jsx
@@ -4,6 +4,29 @@ export function AlphaNumComparison() {
 	return (
 		<section className={styles.comparisonPanel}>
 			<h2>Comparaison Alphanumérique</h2>
+			<div className={styles.alphaNumFontContainer}>
+				<p className={styles.fontName1}>Police 1 -</p>
+			</div>
+			<div className={styles.alphaNumFontContainer}>
+				<p className={styles.fontName2}>Police 2 -</p>
+			</div>
 		</section>
+	);
+}
+
+function AlphaNumElement({ font, weightName }) {
+	return (
+		<div className={styles.alphaNumElementContainer}>
+			<p className={styles.fontWeightName}>{weightName}</p>
+			<p className={styles.alphaNum}>
+				ABCDEFGHIJKLMNOPQRSTUVWXYZ
+				<br />
+				abcdefghijklmnopqrstuvwxyz
+				<br />
+				0123456789
+				<br />
+				!@#$%^&amp;*()
+			</p>
+		</div>
 	);
 }
