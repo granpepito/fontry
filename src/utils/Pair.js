781016696b707f69a81b9fed09085df4553ca056
granpepito <alexandre.goncalvesm@gmail.com>
2023-06-08 14:18:47 +0200

    :fire: Dropped the classes. Now only to help documenting the objects structures.

--- src/utils/Pair.js
+++ src/utils/Pair.js
@@ -1,73 +1,9 @@
-import Font from './Font';
-
 /**
- * Class representing a pair of fonts.
- */
-export default class Pair {
-	#font1;
-	#font2;
-	#theme;
-
-	/**
-	 *  Creates a pair of fonts.
-	 * @param {Font} font1 - The first font.
-	 * @param {Font} font2 - The second font.
-	 */
-	constructor(font1, font2) {
-		this.#font1 = font1;
-		this.#font2 = font2;
-		this.#theme = ['#FFF', '#000'];
-	}
-
-	get font1() {
-		return this.#font1;
-	}
-
-	get font2() {
-		return this.#font2;
-	}
-
-	get theme() {
-		return this.#theme;
-	}
-
-	set font1(font) {
-		this.#font1 = font;
-	}
-
-	set font2(font) {
-		this.#font2 = font;
-	}
-
-	set theme(arrayOfColors) {
-		this.#theme = [...arrayOfColors];
-	}
-
-	/**
-	 * Returns an object containing the first and second font of the pair.
-	 * @returns {PairObject}
-	 */
-	toObject() {
-		return {
-			font1: this.#font1.toObject(),
-			font2: this.#font2.toObject(),
-			theme: this.#theme,
-		};
-	}
-
-	/**
-	 * Returns a stringified object containing the pair's fonts.
-	 * @returns {string}
-	 */
-	toString() {
-		return JSON.stringify(this.toObject());
-	}
-}
-
-/**
- * @typedef PairObject
+ * @typedef Pair
  * @type {object}
- * @property {import("./Font").FontObject} font1 - First font of the pair.
- * @property {import("./Font").FontObject} font2 - Second font of the pair.
- * @property {string[]} theme - Theme used to display the pair.
+ * @property {import("./Font").FontFamily} font1 - First font of the pair.
+ * @property {import("./Font").FontFamily} font2 - Second font of the pair.
+ * @property {string} theme - Theme used to display the pair.
  */
+
+export const Pair = {};
