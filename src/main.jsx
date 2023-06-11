94a7c3659a42978de7dda3a0e6eb02d7f69cad0c
granpepito <alexandre.goncalvesm@gmail.com>
2023-06-11 18:52:57 +0200

    :lipstick: Added fonts using the buildFontStylesheet.js script.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import './serif-fonts.css';
+import './sans-serif-fonts.css';
+import './display-fonts.css';
+import './handwriting-fonts.css';
+import './monospace-fonts.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
