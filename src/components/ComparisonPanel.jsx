c7cf4171232f02576f75f83f7bf5ee9c730885b6
granpepito <alexandre.goncalvesm@gmail.com>
2023-07-04 17:30:23 +0200

    :truck: Renamed files and components for better understanding. :art: Now using the usePair hook instead of receiving the pair from the props and the only current prop serves to know if the section is the current section displayed.

--- src/components/ComparisonPanel.jsx
+++ src/components/ComparisonPanel.jsx
@@ -3,16 +3,15 @@ import { TextualExampleComparison } from './TextualExampleComparison';
 import { usePair } from '../hooks/PairContext';
 
 export function ComparisonPanel({ currentSection }) {
-	const currentPair = usePair();
-	const firstFont = currentPair.font1;
-	const secondFont = currentPair.font2;
-	switch (currentSection) {
-		case 'alphanum': {
-			return <AlphaNumComparison {...{ firstFont, secondFont }} />;
-		}
-		default:
-		case 'textual': {
-			return <TextualExampleComparison {...{ firstFont, secondFont }} />;
-		}
-	}
+	return (
+		<div className={styles.comparisonPanel}>
+			<AlphaNumComparisonSection
+				isCurrentSection={currentSection === 'alphanum'}
+			/>
+			<TextualExampleComparisonSection
+				isCurrentSection={currentSection === 'textual'}
+			/>
+			<CodeSection isCurrentSection={currentSection === 'code'} />
+		</div>
+	);
 }
