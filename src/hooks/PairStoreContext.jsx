2af66e65c2c440c5e7a4257d00d56307e8fe2b04
granpepito <43655487+granpepito@users.noreply.github.com>
2023-07-10 20:18:13 +0200

    ::adhesive_bandage::

--- src/hooks/PairStoreContext.jsx
+++ src/hooks/PairStoreContext.jsx
@@ -124,13 +124,13 @@ function pairStoreReducer(pairStore, action) {
 					return [...pairStore];
 				}
 
-				const updatedPairStore = pairStore.splice(index, 1);
+				pairStore.splice(index, 1);
 				try {
-					localStorage.setItem('pairStore', JSON.stringify(updatedPairStore));
+					localStorage.setItem('pairStore', JSON.stringify([...pairStore]));
 				} catch (error) {
 					console.error(error);
 				}
-				return updatedPairStore;
+				return [...pairStore];
 			}
 			return [...pairStore];
 		}
