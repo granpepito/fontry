169e2e4f21e7508d326fa1cae0046ad3641e6b33
granpepito <43655487+granpepito@users.noreply.github.com>
2023-07-20 19:21:36 +0200

    :art: Better structure of the code and :memo: added documentation.

--- src/functions/loadFont.js
+++ src/functions/loadFont.js
@@ -1,12 +1,23 @@
 import WebFont from 'webfontloader';
 
-export async function loadFont(fontFamily, variants, isLimited) {
+/**
+ * Loads the requested font family. Uses the WebFontLoader package from Typekit.
+ * @param {string} fontFamily - Name of the font family.
+ * @param {string|string[]} variants - Variants of the font family. Defaults to 400 (Regular).
+ * @param {*} isLimited - Flag to indicate that only the characters of the font family's name (e.g. Inter for the Inter font family) is requested.
+ */
+export async function loadFont(
+	fontFamily,
+	variants = '400',
+	isLimited = false
+) {
 	try {
+		/**
+		 * Takes an array of font weights and returns a string with every weight. If variants is a string, the same string is returned.
+		 * @param {string|string[]} variants - Requested weights for the font.
+		 * @returns Returns a formatted string of font weights.
+		 */
 		function formatVariants(variants) {
-			// undefined, null or empty string
-			if (!variants) {
-				return '400';
-			}
 			// Is an array of multiple variants
 			if (Array.isArray(variants)) {
 				return variants.join(',');
@@ -15,11 +26,12 @@ export async function loadFont(fontFamily, variants, isLimited) {
 			return variants;
 		}
 
-		const promise = new Promise((resolve, reject) => {
+		// Using a Promise to asynchroneously get the requested font.
+		new Promise((resolve, reject) => {
 			if (fontFamily) {
-				// console.log(fontFamily);
 				const fontVariants = formatVariants(variants);
 				const text = isLimited ? fontFamily : '';
+
 				WebFont.load({
 					google: {
 						families: [`${fontFamily}:${fontVariants}:latin`],
@@ -32,7 +44,6 @@ export async function loadFont(fontFamily, variants, isLimited) {
 				reject('Font is not defined.');
 			}
 		});
-		return promise;
 	} catch (err) {
 		console.log('HAS ERR', err);
 	}
