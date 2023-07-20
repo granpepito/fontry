583ddcb3aff9e978b841f3aca6189f8513283151
granpepito <43655487+granpepito@users.noreply.github.com>
2023-07-21 00:47:41 +0200

    :art: Changed function variants param.

--- src/functions/loadFont.js
+++ src/functions/loadFont.js
@@ -3,18 +3,18 @@ import WebFont from 'webfontloader';
 /**
  * Loads the requested font family. Uses the WebFontLoader package from Typekit.
  * @param {string} fontFamily - Name of the font family.
- * @param {string|string[]} variants - Variants of the font family. Defaults to 400 (Regular).
+ * @param {string[]} variants - Variants of the font family. Defaults to 400 (Regular).
  * @param {*} isLimited - Flag to indicate that only the characters of the font family's name (e.g. Inter for the Inter font family) is requested.
  */
 export async function loadFont(
 	fontFamily,
-	variants = '400',
+	variants = ['400'],
 	isLimited = false
 ) {
 	try {
 		/**
 		 * Takes an array of font weights and returns a string with every weight. If variants is a string, the same string is returned.
-		 * @param {string|string[]} variants - Requested weights for the font.
+		 * @param {string[]} variants - Requested weights for the font.
 		 * @returns Returns a formatted string of font weights.
 		 */
 		function formatVariants(variants) {
@@ -22,8 +22,6 @@ export async function loadFont(
 			if (Array.isArray(variants)) {
 				return variants.join(',');
 			}
-			// Is a single variant
-			return variants;
 		}
 
 		// Using a Promise to asynchroneously get the requested font.
