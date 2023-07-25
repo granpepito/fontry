9f30ffd4860540896b35ba4e802f61e20df20c25
granpepito <43655487+granpepito@users.noreply.github.com>
2023-07-25 17:11:23 +0200

    :sparkles: New function to download multiple fonts at a time.

--- src/functions/loadFont.js
+++ src/functions/loadFont.js
@@ -30,3 +30,78 @@ export async function loadFont(
 		}
 	});
 }
+
+/**
+ * Loads multiple fonts at a time.
+ * @param {import('../utils/Font').FontFamily[]} fonts - Fonts to download.
+ * @param {boolean} isLimited - Flag to indicate that only the characters of the font family's name (e.g. Inter for the Inter font family) and only the regular variant of the font are requested.
+ * @returns {Promise} Returns as simple description to know if there are any errors or if the fonts have been downloaded.
+ */
+export async function loadMultipleFonts(fonts, isLimited = false) {
+	if (!fonts || !Array.isArray(fonts)) {
+		return new Promise((_, reject) => reject('There are no fonts.'));
+	}
+
+	let families = [];
+	for (let i = 0; i < fonts.length; i++) {
+		const { family, variants } = fonts[i];
+
+		let formattedFont;
+		if (isLimited) {
+			formattedFont = formatFont(family, reduceVariants(variants));
+		}
+		formattedFont = formatFont(family, variants);
+
+		if (formattedFont) {
+			families.push(formattedFont);
+		}
+	}
+
+	if (families.length === 0) {
+		return new Promise((_, reject) => reject('There are no fonts.'));
+	}
+
+	return new Promise((resolve, reject) => {
+		WebFont.load({
+			google: {
+				families,
+				text: isLimited
+					? 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+					: '',
+			},
+			active: () => resolve(`Loading result of multiple fonts: 200`),
+			inactive: () => reject(`Loading result of multiple fonts: 404`),
+		});
+	});
+}
+
+/**
+ * Render a string in the format used by the "webfontloader" package for its "families" property.
+ * @param {string} family - Font family name.
+ * @param {string[]} variants - Variants of the fonts to format.
+ * @returns {string|null} Returns a string in the following format <family>:<variant,variant,...>. Returns null if the family is falsy or the variants is not an array.
+ */
+function formatFont(family, variants) {
+	if (!family) {
+		return null;
+	}
+
+	if (Array.isArray(variants)) {
+		let formattedFont = `${family}:${variants.join(',')}`;
+
+		return formattedFont;
+	}
+	return null;
+}
+
+/**
+ * Reduces the given array if it contains the "regular" font weight variant.
+ * @param {string[]} variants
+ * @returns {string[]} Returns an array containing one element that is the regular (400) font weight. If the given array does not contain the "regular" variant, it returns the same array unchanged.
+ */
+function reduceVariants(variants) {
+	if (variants.includes('regular')) {
+		return ['regular'];
+	}
+	return variants;
+}
