9bc72462bc59feb7b84e4282c20a40e14cca1d2c
granpepito <43655487+granpepito@users.noreply.github.com>
2023-08-03 13:17:56 +0200

    :pencil2: Corrected the name of VariantCheckBox.

--- src/components/CodeSection.jsx
+++ src/components/CodeSection.jsx
@@ -71,14 +71,14 @@ function WeightsSelector({ firstFont, secondFont, isDisabled }) {
 	const firstVariants = useMemo(() => {
 		const { family, variants } = firstFont;
 		return variants.map((variant, index) => {
-			return <VariantCheckBock key={index} {...{ family, variant }} />;
+			return <VariantCheckbox key={index} {...{ family, variant }} />;
 		});
 	}, [firstFont]);
 
 	const secondVariants = useMemo(() => {
 		const { family, variants } = secondFont;
 		return variants.map((variant, index) => {
-			return <VariantCheckBock key={index} {...{ family, variant }} />;
+			return <VariantCheckbox key={index} {...{ family, variant }} />;
 		});
 	}, [secondFont]);
 
@@ -96,7 +96,7 @@ function WeightsSelector({ firstFont, secondFont, isDisabled }) {
 	);
 }
 
-function VariantCheckBock({ family, variant }) {
+function VariantCheckbox({ family, variant }) {
 	const [isChecked, setIsChecked] = useState(true);
 	const activeClassName = isChecked ? inputStyles.active : '';
 
