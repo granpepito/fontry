95478e528cf373ab96c67fd7d4bc0ee7ff57b096
granpepito <43655487+granpepito@users.noreply.github.com>
2023-08-11 16:51:31 +0200

    :sparkles: Added Footer to App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header';
 import { Main } from './components/Main';
+import { Footer } from './components/Footer';
 import { PairProvider } from './hooks/PairContext';
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
 			<PairProvider>
 				<Header />
 				<Main />
+				<Footer />
 			</PairProvider>
 		</>
 	);
