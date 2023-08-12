27647dab4372376f977ccfae5c01e3bb74904157
granpepito <43655487+granpepito@users.noreply.github.com>
2023-08-12 18:47:53 +0200

    :lipstick: Removed the Github icon and centered everything.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,33 +3,20 @@ import { CodeBracketIcon } from '@heroicons/react/24/outline';
 import styles from '/src/assets/styles/footer.module.css';
 import iconStyles from '/src/assets/styles/icon.module.css';
 
-import githubIcon from '/src/assets/img/github.svg';
-
 export function Footer() {
 	return (
 		<footer className={styles.footer}>
 			<p>
 				<CodeBracketIcon
-					className={[iconStyles.smallIcon, styles.pencil].join(' ')}
+					className={[iconStyles.smallIcon, styles.bracket].join(' ')}
 				/>
 			</p>
 			<p className={styles.dotDev}>
 				<a href='https://alexgm.dev'>alexgm.dev</a>
 			</p>
-			<ul>
-				<li>
-					<a href='https://www.github.com/granpepito/fontry'>
-						<img
-							className={[iconStyles.smallIcon, styles.githubIcon].join(' ')}
-							src={githubIcon}
-							alt='GitHub Logo'
-						/>
-					</a>
-				</li>
-				<li>
-					<a href='https://www.github.com/granpepito'>granpepito</a>
-				</li>
-			</ul>
+			<p>
+				<a href='https://www.github.com/granpepito/fontry'>GitHub</a>
+			</p>
 		</footer>
 	);
 }
