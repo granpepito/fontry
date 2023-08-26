b7ef02c0e2f3e9d6a872f3406466e0fecaa38812
granpepito <43655487+granpepito@users.noreply.github.com>
2023-08-26 18:27:58 +0200

    :heavy_plus_sign: Added lightningcss dependency.
    :wrench: Using lightiningcss for css modules and minify.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			cssModules: {
+				pattern: '[name]-[hash]-[local]',
+			},
+		},
+	},
+	build: {
+		cssMinify: 'lightningcss',
+	},
 	plugins: [
 		react(),
 		svgr({
