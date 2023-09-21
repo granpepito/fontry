3f8a310d56b339ad4d2e48427daef81c7b85b0dd
granpepito <43655487+granpepito@users.noreply.github.com>
2023-09-21 16:10:13 +0200

    🩹 Removed javascript: URLs

--- src/components/TextualExampleComparisonSection.jsx
+++ src/components/TextualExampleComparisonSection.jsx
@@ -121,7 +121,7 @@ function TextualElement({ fontFamily, type, isDisabled = false }) {
 							Un Bouton Inutile
 						</button>
 						<span>
-							<a href='javascript:void(0)'>
+							<a href='#' onClick={(e) => e.preventDefault()}>
 								<ArrowLongRightIcon
 									className={[iconStyles.smallIcon, styles.icon].join(' ')}
 								/>
@@ -129,7 +129,7 @@ function TextualElement({ fontFamily, type, isDisabled = false }) {
 							</a>
 						</span>
 						<span>
-							<a href='javascript:void(0)'>
+							<a href='#' onClick={(e) => e.preventDefault()}>
 								<ArrowLongRightIcon
 									className={[iconStyles.smallIcon, styles.icon].join(' ')}
 								/>
